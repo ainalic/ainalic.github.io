5ac49dc7dd10eb599da5d64f6167b201d4854d14
Chrysanthi Ainali <cainali@Chrysanthi-Ainali-MacBook-Pro.local>
2024-04-24 13:14:36 +0100

    only markdown

--- README.md
+++
@@ -1,1 +0,0 @@
-# ainalic.github.io
--- script.js
+++
@@ -1,4 +0,0 @@
-// Example JavaScript to interact with the document
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Document loaded!');
-});
